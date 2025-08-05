3e64dc5b1588c01001a827ad065c64695f280290
Alexandra Gutierrez <59211024+alexndra1272@users.noreply.github.com>
2025-08-05 22:02:26 +0000

    style: add style for new page form

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
 class PageForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': 1,
-        'cols': 1
-    }))
+    title = forms.CharField(max_length=100, label="Título de la página", widget=forms.TextInput(attrs={
+            'placeholder': "Ej: JavaScript, Python, Django...",
+            'class': 'border border-gray-300 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight mt-2 mb-6 focus:outline-none focus:border-blue-500'
+        }))
+    content = forms.CharField(label="Contenido de la página", widget=forms.Textarea(
+        attrs={
+            'rows': 12, 
+            'placeholder': "## Hola mundo.",
+            'class': 'border border-gray-300 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight mt-2 mb-4 focus:outline-none focus:border-blue-500'
+        }
+    ))
