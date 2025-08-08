8f6f89a648223bb14251b1719226a602a6228e2a
Alexandra Gutierrez <59211024+alexndra1272@users.noreply.github.com>
2025-08-08 19:55:50 +0000

    chore: add url from render on allowed host

--- wiki/settings.py
+++ wiki/settings.py
@@ -31,6 +31,10 @@ DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
